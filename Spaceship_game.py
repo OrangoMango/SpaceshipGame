453f7e4ee534a520569289c2c40174ef285e5ee5
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-03-22 20:23:27 +0100

    Update Spaceship_game.py
    Modified wall shape and added runtime variable.

--- Spaceship_game.py
+++ Spaceship_game.py
@@ -22,6 +22,7 @@ class Game:
 		while True:
 			w.draw()
 			s.draw()
+			self.canvas.tag_raise(self.scoretext)
 			self.tk.update()
 			time.sleep(0.01)
 	def get_pointer_coord(self):
@@ -33,7 +34,7 @@ class Game:
 		center = sx+((sx1-sx)/2), sy+((sy1-sy)/2)
 		#print("Center:", center)
 
-		pcoord = [[(sx+((sx1-sx)/2), sy),(sx1, sy),(sx1, sy1),(sx, sy1),(sx,sy+((sy1-sy)/2)),center], #area1
+		pcoord = [[(sx+((sx1-sx)/2), sy),(sx1, sy),(sx1, sy1),(sx, sy1),(sx,sy+((sy1-sy)/2)),center],           #area1
 							[(sx,sy),(sx+((sx1-sx)/2),sy),center,(sx1,sy+((sy1-sy)/2)),(sx1,sy1),(sx,sy1)],     #area2
 							[(sx,sy),(sx1,sy),(sx1,sy+((sy1-sy)/2)),center,(sx+((sx1-sx)/2),sy1),(sx,sy1)],     #area3
 							[(sx,sy),(sx1,sy),(sx1, sy1),(sx+((sx1-sx)/2), sy1),center,(sx,sy+((sy1-sy)/2))]    #area4
@@ -49,7 +50,6 @@ class Wall:
 		self.x, self.y, self.x1, self.y1 = x-w, y-w, x+w, y+w
 		self.correct = random.randint(0,3) #Options are from 0 to 3 inclusive
 		self.id = self.game.canvas.create_polygon(self.game.get_coord_area([self.x, self.y, self.x1, self.y1], self.correct), fill="blue")
-		#print("Correct:", self.correct)
 	def draw(self):
 		if not self.x <= 8:
 			self.x -= self.runtime
@@ -65,7 +65,6 @@ class Wall:
 			self.game.score += 1
 			self.game.canvas.itemconfig(self.game.scoretext, text="Score: {}".format(self.game.score))
 			self.correct = random.randint(0,3)
-			#print("Correct:", self.correct)
 		self.game.canvas.delete(self.id)
 		self.id = self.game.canvas.create_polygon(self.game.get_coord_area([self.x, self.y, self.x1, self.y1], self.correct), fill="blue")
 	def add_sp(self, s):
@@ -75,7 +74,6 @@ class Spaceship:
 	def __init__(self, game, x, y, x1, y1):
 		self.game = game
 		self.id = self.game.canvas.create_rectangle(x, y, x1, y1, fill="red")
-		#self.game.canvas.tag_bind(self.id, "<Button-1>", self.onclick)
 	def check_area(self):
 		p = self.game.canvas.coords(self.id)
 		areas = [p[2] < 250 and p[3] < 250,
@@ -83,8 +81,6 @@ class Spaceship:
 						p[0] > 250 and p[1] > 250,
 						p[0] < 250 and p[3] > 250
 						]
-		#print(areas)
-		#print(areas.index(True))
 		try:
 			return areas.index(True)
 		except:
@@ -98,7 +94,7 @@ class Spaceship:
 
 if __name__ == '__main__':
 	g = Game()
-	runtime = 3.5
+	runtime = 5 #Yo can modify this line for a game more difficult
 	w = Wall(g, 250, 250, runtime=runtime) #width = 5
 	s = Spaceship(g, 10, 10, 60, 60) #width = 50
 	w.add_sp(s)
